7dd94b791d3845c8761b1c07ee2bfa0dd92a5787
Thiago Rodrigues <ti.thiago.rodrigues@outlook.com>
2023-11-15 15:19:16 -0300

    refactor: remove imports

--- transactions/src/handlers/sqsReportsTransactions.ts
+++ transactions/src/handlers/sqsReportsTransactions.ts
@@ -1,20 +1,16 @@
-import { ReportsTransactionsRepository } from './../repository/reportsTransactions.repository'
-import { APIGatewayProxyEvent, APIGatewayProxyResult, SQSEvent, Context } from 'aws-lambda'
-import { TransactionRepository } from '../repository/transactions.repository'
-import { CreateCore } from '../core/create.core'
-import { AppErrorException, formatResponse } from '../utils'
-import { CreateTransactionsType } from '../shared/types'
-import { CreateReportsTransactionCore } from '../core/createReportsTransaction.core'
+import { ReportsTransactionsMonthlyRepository } from '../repository/reportsTransactionsMonthly.repository'
+import { SQSEvent, Context } from 'aws-lambda'
+import { CreateReportsTransactionMonthlyCore } from '../core/createReportsTransactionMonthly.core'
 
-export const handler = async (event: SQSEvent, context: Context): PromiseSettledResult<void> => {
-  const repository = new ReportsTransactionsRepository()
-  const createReportsTransactionCore = new CreateReportsTransactionCore(repository)
+export const handler = async (event: SQSEvent, context: Context): Promise<PromiseSettledResult<void>[]> => {
+  const repository = new ReportsTransactionsMonthlyRepository()
+  const createReportsTransactionMonthlyCore = new CreateReportsTransactionMonthlyCore(repository)
 
   const recordPromises = event.Records.map(async (record) => {
     console.log(`MessageId: ${record.messageId} `)
     // usar outra coisa sem ser JSON.parse
     const body = JSON.parse(record.body)
-    await createReportsTransactionCore.execute(body)
+    await createReportsTransactionMonthlyCore.execute(body)
   })
   const resultPromises = await Promise.allSettled(recordPromises)
   console.debug('resultPromises: ', resultPromises)
