0fad686cc505d8f1ec3b17d80c58ba44f1b03a3d
Thiago Rodrigues <ti.thiago.rodrigues@outlook.com>
2024-01-06 23:58:49 -0300

    feat: added filters category and card

--- bot-telegram/src/core/processMessage.core.ts
+++ bot-telegram/src/core/processMessage.core.ts
@@ -1,12 +1,12 @@
 import { AppErrorException } from '../utils'
-import { errorMessages } from '../shared/constants/errorMessages'
+import { messages } from '../shared/constants/messages'
 import { ProcessMessageType } from '../shared/types'
 import { parseCommand } from '../utils/parseCommand'
 import { listCommands } from '../shared/constants/listCommands'
-import { randomUUID } from 'crypto'
+import { CreateTransactionTelegramType } from '../shared/types/createTransactionFromTelegram.type'
 
 export class ProcessMessageCore {
-  async execute({ message, user }: ProcessMessageType) {
+  async execute({ message, user }: ProcessMessageType): Promise<{ transaction: CreateTransactionTelegramType }> {
     console.info('call ProcessMessage')
     // Envio de um SQS Update para o User quando verificar que é o primeiro acesso
     try {
@@ -14,26 +14,20 @@ export class ProcessMessageCore {
       const cmd = messageCommands?.cmd.toLocaleLowerCase()
       if (!cmd || !listCommands.includes(cmd ?? '') || !messageCommands?.tokens) {
         console.error('Invalid Command')
-        throw new AppErrorException(400, errorMessages.command_not_found)
+        throw new AppErrorException(400, messages.command_not_found)
       }
 
       const attributes = messageCommands.tokens.filter((token) => token !== '-')
 
       if (attributes.length < 4) {
-        throw new AppErrorException(400, errorMessages.command_invalid)
+        throw new AppErrorException(400, messages.command_invalid)
       }
       return {
         transaction: {
           date: attributes[0],
-          category: {
-            id: randomUUID(),
-            name: attributes[1],
-          },
-          card: {
-            id: randomUUID(),
-            name: attributes[2],
-          },
-          value: attributes[3],
+          categoryName: attributes[1],
+          value: attributes[2],
+          cardName: attributes[3],
           description: attributes[4] ?? '',
           userId: user.userId,
           type: messageCommands?.cmd.toLowerCase() === '/adicionarreceita' ? '+' : '-',
@@ -42,7 +36,7 @@ export class ProcessMessageCore {
       }
     } catch (error: any) {
       console.error('err', error)
-      throw new Error(error?.message ?? errorMessages.command_invalid)
+      throw new Error(error?.message ?? messages.command_invalid)
     }
   }
 }
