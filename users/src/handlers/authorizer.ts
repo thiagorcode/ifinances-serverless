cbab3c4c607d9431a2059a59f033e699fafe5484
Thiago Rodrigues <ti.thiago.rodrigues@outlook.com>
2024-01-28 22:46:35 -0300

    feat: added authorizer

--- users/src/handlers/authorizer.ts
+++ users/src/handlers/authorizer.ts
@@ -1,56 +1,15 @@
-import { APIGatewayAuthorizerResult, APIGatewayTokenAuthorizerEvent } from 'aws-lambda'
+import { APIGatewayRequestAuthorizerEvent, APIGatewaySimpleAuthorizerResult } from 'aws-lambda'
 import { AuthorizerCore } from '../core/authorizer.core'
 
-export const handler = async (event: APIGatewayTokenAuthorizerEvent): Promise<APIGatewayAuthorizerResult> => {
+export const handler = async (event: APIGatewayRequestAuthorizerEvent): Promise<APIGatewaySimpleAuthorizerResult> => {
   try {
     console.debug('Event:', event)
     const authorizerCore = new AuthorizerCore()
-    const statusAccess = await authorizerCore.execute(event.authorizationToken)
-
-    if (statusAccess) {
-      return {
-        principalId: 'user',
-        policyDocument: {
-          Version: '2012-10-17',
-          Statement: [
-            {
-              Action: 'execute-api:Invoke',
-              Effect: 'Allow',
-              Resource: event.methodArn,
-            },
-          ],
-        },
-      }
-    }
-
-    return {
-      principalId: 'user',
-      policyDocument: {
-        Version: '2012-10-17',
-        Statement: [
-          {
-            Action: 'execute-api:Invoke',
-            Effect: 'Deny',
-            Resource: event.methodArn,
-          },
-        ],
-      },
-    }
+    const statusAccess = await authorizerCore.execute(event.headers?.authorization ?? '')
+    console.info('status', statusAccess)
+    return { isAuthorized: statusAccess }
   } catch (err) {
     console.error('handler error', err)
-
-    return {
-      principalId: 'user',
-      policyDocument: {
-        Version: '2012-10-17',
-        Statement: [
-          {
-            Action: 'execute-api:Invoke',
-            Effect: 'Deny',
-            Resource: event.methodArn,
-          },
-        ],
-      },
-    }
+    return { isAuthorized: false }
   }
 }
